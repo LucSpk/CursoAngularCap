eed316b17a222750ea2e5d70c0da3417a78dedb2
Lucas Alves <olalvezluc@gmail.com>
2022-04-30 18:14:46 -0300

    Subindo Projeto

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -19,13 +19,35 @@ export class CursosComponent implements OnInit {
 
   // - Inicialização
   ngOnInit(): void {
+    this.id = -1;
     this.curso = new Curso();
     this.vetorDeCursos = this.servico.listar();
   }
 
-  //this.servico.cadastrar(this.curso)
   cadastrar() {
     this.servico.cadastrar(this.curso);
     this.curso = new Curso();
+  }
+
+  excluir(id:number) {
+    this.servico.excluir(id);
+    this.id = -1;
+  }
+
+  editar(id:number) {
+
+    this.id = id;
+
+    this.curso = new Curso(
+      this.vetorDeCursos[id].nomeCurso,
+      this.vetorDeCursos[id].valorCurso,
+      this.vetorDeCursos[id].areaCurso
+    );
+  }
+
+  atualizar() {
+    this.servico.alterar(this.id,this.curso);
+    this.curso = new Curso();
+    this.id = -1;
   }
 }
